242182a923ebc9d1540505561b4f1c85037fef4a
Jonas Hartmann <80170229+hartmann-jonas@users.noreply.github.com>
2022-12-14 09:52:02 +0100

    Finished chat side, ready to hand in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,74 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { enhance } from "$app/forms";
+    import { fly } from "svelte/transition"
+    import { beforeNavigate, invalidate } from "$app/navigation";
+    import "@picocss/pico";
+    import "elizabot";
+    import ElizaBot from "elizabot";
+    beforeNavigate(() => {
+      invalidate(); // force csr to "unload" the imported css on this page
+      // try commenting this out and navigate using href links and see how the
+      // picocss is carried with us to other pages. its an ugly hack.
+    });
+
+    let eliza = new ElizaBot();
+    let chat = [{ user: "eliza", text: eliza.getInitial() }];
+
+    async function write(message) {
+        console.log(message);
+        chat.push({user: "user", text: message});
+        chat.push({user: "eliza", text: eliza.transform(message)})
+        console.log(chat)
+        chat = chat
+        return chat;
+    }
+  </script>
+  
+  <div class="container">
+    <h1>Chat with Eliza</h1>
+    <div class="scrollable">
+        <div class="chat">
+            <!-- TODO: loop over the messages and display them -->
+            {#each chat as message}
+            <article>
+                <span>
+                    {message.text}
+                </span>
+            </article>
+            {/each}
+        </div>
+    </div>
+
+    <form
+        method="post"
+      use:enhance={({ form, data, action, cancel }) => {
+        /* https://kit.svelte.dev/docs/form-actions#progressive-enhancement */
+        cancel(); //don't post anything to server
+        const text = data.get("text");
+        write(text);
+        form.reset();
+      }}
+    >
+      <input type="text" name="text"/>
+    </form>
+  </div>
+
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap');
+
+    :root {
+        --font-family: 'Roboto Mono', monospace;
+    }
+
+    .container {
+        padding-top: 20px;
+        height: 100vh;
+    }
+
+    .scrollable {
+        height: 70%;
+        overflow: auto;
+        display: flex;
+        flex-direction: column-reverse;
+    }
+  </style>
